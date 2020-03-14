b62bd0205041b00649edd07ed9df748d49c640f1
slice3984 <->
2020-03-14 23:00:36 +0100

    config parsing / warsow config class

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,5 +1,8 @@
 import { ConfigParser } from './config-parser'
+import { WarsowConfig } from './warsow-config';
 
 document.getElementById('open-config').addEventListener('change', () => {
-    ConfigParser.parse();
+    ConfigParser.parse().then((cfg: WarsowConfig) => {
+        const config = cfg;
+    });
 });
--- src/ts/config-parser.ts
+++ src/ts/config-parser.ts
@@ -1,4 +1,4 @@
-import { configProperty } from './warsow-config'
+import { configProperty, WarsowConfig } from './warsow-config'
 
 export class ConfigParser {
 	private static importBtnEl = document.getElementById('open-config') as HTMLInputElement;
@@ -6,49 +6,54 @@ export class ConfigParser {
 	private constructor() { }
 
 	static parse() {
-		const config = this.importBtnEl.files[0];
-		const reader = new FileReader();
-		reader.readAsText(config, 'utf-8');
-		reader.onload = e => {
-			// Store as array and remove comments + empty lines + spacings
-			const lines = (e.target.result as string)
-				.split('\n')
-				.filter(line => {
-					return !line.trim().startsWith('//') &&
-						!(line.trim() === '')
-				})
-				.map(line => line.trim());
+		return new Promise((res, rej) => {
+			const config = this.importBtnEl.files[0];
+			const reader = new FileReader();
+			reader.readAsText(config, 'utf-8');
+			reader.onload = e => {
+				// Store as array and remove comments + empty lines + spacings
+				const lines = (e.target.result as string)
+					.split('\n')
+					.filter(line => {
+						return !line.trim().startsWith('//') &&
+							!(line.trim() === '')
+					})
+					.map(line => line.trim());
 
-			const binds: configProperty[] = [];
-			const setas: configProperty[] = [];
-			const misc: configProperty[] = [];
+				const binds: configProperty[] = [];
+				const setas: configProperty[] = [];
+				const misc: configProperty[] = [];
 
-			lines.forEach(line => {
-				const parts = line.split(' ');
+				lines.forEach(line => {
+					const parts = line.split(' ');
 
-				// unbindall, unaliasall
-				if (parts.length <= 2) {
-					return;
-				}
+					// unbindall, unaliasall
+					if (parts.length <= 2) {
+						return;
+					}
 
-				const type = parts[0];
-				const property = parts[1];
-				let value = parts.slice(2, parts.length).join(" ");
-				value = value.replace(/^\"|\"$/g, "");
+					const type = parts[0];
+					const property = parts[1];
+					let value = parts.slice(2, parts.length).join(" ");
+					value = value.replace(/^\"|\"$/g, "");
 
-				const containsColors = /\^[0-9]/.test(value);
+					const containsColors = /\^[0-9]/.test(value);
 
-				switch (type) {
-					case 'bind':
+					switch (type) {
+						case 'bind':
+							binds.push({ type, property, value, containsColors });
+							break;
+						case 'seta':
+							setas.push({ type, property, value, containsColors });
+							break;
+						default:
+							misc.push({ type, property, value, containsColors });
+					}
+				});
 
-						break;
-					case 'seta':
-
-						break;
-					default:
-
-				}
-			});
-		};
+				// console.log(binds, setas, misc);
+				res(new WarsowConfig(binds, setas, misc));
+			};
+		})
 	}
 }
